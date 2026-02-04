ed6c8e12a493d28b480d11d8b36d388e74fce0fd
Addison Pirlo <addisonpirlo2@gmail.com>
2026-02-03 19:05:56 -0500

    fix: update KMS key policy to allow caller administration

--- kms.tf
+++ kms.tf
@@ -3,37 +3,46 @@ resource "aws_kms_key" "site_key" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "EnableRootPermissions"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
-        Action   = "kms:*"
-        Resource = "*"
-      },
-      {
-        Sid    = "AllowCloudFrontLogging"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action = [
-          "kms:Encrypt",
-          "kms:GenerateDataKey*"
-        ]
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
-          }
+policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [
+    {
+      Sid    = "EnableRootPermissions"
+      Effect = "Allow"
+      Principal = {
+        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      }
+      Action   = "kms:*"
+      Resource = "*"
+    },
+    {
+      Sid    = "AllowKeyAdministration"
+      Effect = "Allow"
+      Principal = {
+        AWS = data.aws_caller_identity.current.arn
+      }
+      Action   = "kms:*"
+      Resource = "*"
+    },
+    {
+      Sid    = "AllowCloudFrontLogging"
+      Effect = "Allow"
+      Principal = {
+        Service = "cloudfront.amazonaws.com"
+      }
+      Action = [
+        "kms:Encrypt",
+        "kms:GenerateDataKey*"
+      ]
+      Resource = "*"
+      Condition = {
+        StringEquals = {
+          "aws:SourceAccount" = data.aws_caller_identity.current.account_id
         }
       }
-    ]
-  })
+    }
+  ]
+})
 
   tags = local.common_tags
 }
